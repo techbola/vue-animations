0376ed3323c77076a0ac80886de77aa6f1e54813
Adebola <bola@neegles.com>
2024-11-18 16:23:30 +0100

    project setup

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
+      inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a
+      dolore consequatur assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
+      inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a
+      dolore consequatur assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
+      inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a
+      dolore consequatur assumenda nam!
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Toast v-if="showToast" />
+    <Todos @badValue="triggerToast" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/ToastComponent.vue'
+import Todos from '../components/TodosComponent.vue'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true
+      setTimeout(() => (showToast.value = false), 3000)
+    }
+
+    return { showToast, triggerToast }
+  },
+}
+</script>
+
+<style></style>
